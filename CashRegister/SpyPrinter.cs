4db9948f08c3935ac2fd3f19591ea5ab9eb7641e
Bingqi <BLiu25@slb.com>
2022-10-26 15:02:28 +0800

    test(question2): add test double for printer.

---
+++ CashRegister/SpyPrinter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashRegister
+{
+    public class SpyPrinter : Printer
+    {
+        public bool HasPrinted { get; set; }
+        public override void Print(string content)
+        {
+            // send message to a real printer
+            base.Print(content);
+            HasPrinted = true;
+        }
+    }
+}
